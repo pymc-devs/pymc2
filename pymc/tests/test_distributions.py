4247e510d9ef068560448e680cebd277f721da7e
Kyle Beauchamp <kyleabeauchamp@gmail.com>
2016-04-29 23:01:51 -0700

    Add two more tests

--- pymc/tests/test_distributions.py
+++ pymc/tests/test_distributions.py
@@ -439,6 +439,10 @@ class test_betabin(TestCase):
         assert_equal(np.exp(flib.betabin_like(-1, a, b, n)), 0.0)
         assert_equal(np.exp(flib.betabin_like(n + 1, a, b, n)), 0.0)
 
+        a, b = 5, 5
+        assert_equal(np.exp(flib.betabin_like(-1, a, b, n)), 0.0)
+        assert_equal(np.exp(flib.betabin_like(n + 1, a, b, n)), 0.0)
+
 
 class test_categorical(TestCase):
 
